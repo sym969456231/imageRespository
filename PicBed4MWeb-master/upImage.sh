96d4c7594a735619bba661306d5b0f593b1c8d99
gongkuihua <gkh60@foxmail.com>
2020-05-11 21:25:10 +0800

    图片上传

--- PicBed4MWeb-master/upImage.sh
+++ PicBed4MWeb-master/upImage.sh
@@ -5,6 +5,7 @@ git config --global user.email 'gkh60@foxmail.com'
 #     git clone https://github.com/gongkuihua/imageRespository
 # fi
 
+git pull
 git add .
 git commit -m "图片上传"
 git remote add origin https://github.com/gongkuihua/imageRespository
